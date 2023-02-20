03a9351717e1ea963ec22146c1a7d8febeb7f401
Danny Chan <z5453114@ad.unsw.edu.au>
2023-02-20 20:07:12 +1100

    implementing channel functions

---
+++ src/channel.js
@@ -0,0 +1,28 @@
+function channelDetailsV1(authUserId, channelId) {
+    return {
+        name: 'Hayden',
+        ownerMembers: [
+          {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+          }
+        ],
+        allMembers: [
+          {
+            uId: 1,
+            email: 'example@gmail.com',
+            nameFirst: 'Hayden',
+            nameLast: 'Jacobs',
+            handleStr: 'haydenjacobs',
+          }
+        ],
+      };
+}
+
+function channelJoinV1(authUserId, channelId) {
+    return {};
+}
+ 
