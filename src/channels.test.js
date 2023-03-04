db673450f8bd722136c23a90acf9c7040fb35975
ali <z5387428@ad.unsw.edu.au>
2023-03-04 18:32:34 +1100

    added tests for channelsCreateV1

---
+++ src/channels.test.js
@@ -0,0 +1,29 @@
+import {channelsCreateV1}  from 'channels.js';
+
+const ERROR = { error: expect.any(String) };
+
+describe('Checking input error cases for channelsCreateV1', () => {
+  test('Empty channel name', () => {
+    expect(channelsCreateV1(1, '', true)).toStrictEqual({ERROR});
+  });
+
+  test('Channel name more than 20 characters', () => {
+    expect(channelsCreateV1(1, 'qwertyuioplkjhgfdsazx', true)).toStrictEqual({ERROR});
+  });
+
+  test('Invalid authUserId', () => {
+    let result = authRegisterV1('validemail@gmail.com', '123abc!@#', 'Jake', 'Renzella');
+
+    expect(channelsCreateV1(result + 1, 'validChannel', true)).toStrictEqual({ERROR});
+  });
+});
+
+describe('Checking valid input for channelsCreateV1', () => {
+  
+  test('correct input', () => {
+    expect(channelsCreateV1(1, 'validChannel', true)).toStrictEqual(expect.any(Number));
+  });
+
+});
+
+  
