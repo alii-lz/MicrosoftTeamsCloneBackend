b50fbafe361d0e03f50dd33df8a85a2f13036bfb
Matthew Ieong <z5391699@ad.unsw.edu.au>
2023-03-09 04:51:41 +1100

    Implementing the channelsListAllV1 function

--- src/channels.js
+++ src/channels.js
@@ -1,3 +1,5 @@
+import { getData } from "./dataStore";
+
 function channelsCreateV1(authUserId, name, isPublic) {    
   return { channelId: 1 }
 }
@@ -13,13 +15,25 @@ function channelsListV1(authUserId) {
   }
 }
 
-function channelsListAllV1(authUserId) {  
-  return {
-    channels: [
-      {
-        channelId: 1,
-        name: 'My Channel',
-      }
-    ],
+function channelsListAllV1(authUserId) {
+  //need adjustments
+  const data = getData();
+  const channels = [];
+
+  if (authUserId == '') {
+    return { error: 'empty authUserId' };
+  } else if (authUserId < 0 || authUserId >= data.length) {
+    return { error: 'invalid authUserId' };
   }
+
+  for (const channel of data.channels) {
+    const current_channel = {
+      channelId: channel.channelId,
+      name: channel.name,
+    };
+    channels.push(current_channel);
+  }  
+    return { channels };
 }
+
+export {channelsListAllV1, channelsListV1, channelsCreateV1};
