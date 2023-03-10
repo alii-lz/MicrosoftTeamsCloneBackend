21fc438c2bf658bb7c771da4bf55e6e767e32dd1
Rudransh Randeria <z5396697@ad.unsw.edu.au>
2023-03-10 20:59:27 +1100

    Rectified minor syntax errors in the test functions

--- src/auth.test.js
+++ src/auth.test.js
@@ -5,7 +5,7 @@ import { ClearV1 } from './other';
 // Tests for AuthLoginV1
 test('Test Case 1: Successful Login ', () => {
     ClearV1();
-    const registeredUser = authRegisterV1('rudie@gmail.com', 'soccer7');
+    const registeredUser = authRegisterV1('rudie@gmail.com', 'soccer7', 'rudie', 'tate');
     const loginUser = authLoginV1('rudie@gmail.com', 'soccer7');
 
     expect(registeredUser).toStrictEqual(loginUser);
@@ -13,7 +13,7 @@ test('Test Case 1: Successful Login ', () => {
 
 test('Test Case 2: Unsuccessful Login : Invalid Email ', () => {
     ClearV1();
-    authRegisterV1('rudie@gmail.com', 'soccer7');
+    authRegisterV1('rudie@gmail.com', 'soccer7', 'rudie', 'tate');
     const loginUser = authLoginV1('rudie07@gmail.com', 'soccer7');
 
     expect(loginUser).toHaveProperty('error');
@@ -21,7 +21,7 @@ test('Test Case 2: Unsuccessful Login : Invalid Email ', () => {
 
 test('Test Case 3: Unsuccessful Login : Invalid Password ', () => {
     ClearV1();
-    authRegisterV1('rudie@gmail.com', 'soccer7');
+    authRegisterV1('rudie@gmail.com', 'soccer7', 'rudie', 'tate');
     const loginUser = authLoginV1('rudie@gmail.com', 'Soccer7');
 
     expect(loginUser).toHaveProperty('error');
