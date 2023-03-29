92879ca245eed10f375c39f251785c59683ad2c2
Rudransh Randeria <z5396697@ad.unsw.edu.au>
2023-03-30 04:40:34 +1100

    Implemented tests for authRegister

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,41 +1,27 @@
-export interface error {
-error: string
+export interface User{
+    uId: number,
+    nameFirst:string,
+    nameLast: string,
+    password: string,
+    email: string,
+    handleStr: string,
+    globalOwner: boolean
+
+}
+export interface Channels{
+    channelId: number,
+    name: string,
+    isPublic: boolean,
+    owners: object[],
+    allMembers: object[]
 }
 
-export interface authUserId {
-authUserId: number
+export interface Token{
+    token: string,
+    active: boolean
 }
-
-export interface channelDetails {
-name: string,
-isPublic: string,
-ownerMembers: any[],
-allMembers: any[]
-}
-
-export interface channelMessages {
-messages: any[],
-start: number,
-end: number
-}
-
-export interface channelId {
-channelId: number
-}
-
-export interface channelsTemp {
-channelId: number,
-name: string,
-}
-
-export interface channels {
-channels: channelsTemp[]
-}
-
-export interface user {
-uId: number,
-email: string,
-nameFirst: string,
-nameLast: string,
-handleStr: string
-}
+export interface Data {
+    users: User[];
+    channels: Channels[];
+    tokens: Token[];
+  }
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { echo } from './echo';
 import morgan from 'morgan';
 import config from './config.json';
 import cors from 'cors';
+import { authRegisterV1 } from './auth';
+import { clearV1 } from './other';
 
 // Set up web app
 const app = express();
@@ -17,9 +19,23 @@ const PORT: number = parseInt(process.env.PORT || config.port);
 const HOST: string = process.env.IP || 'localhost';
 
 // Example get request
-app.get('/echo', (req: Request, res: Response, next) => {
+app.get('/echo', (req: Request, res: Response) => {
   const data = req.query.echo as string;
   return res.json(echo(data));
+});
+app.delete('/clear/v1', (req: Request, res: Response) => {
+  return res.json(clearV1());
+});
+
+app.post('/auth/register/v2', (req: Request, res: Response) => {
+  interface AuthRegisterInput{
+    email: string,
+    password: string,
+    nameFirst: string,
+    nameLast: string
+  }
+  const data = req.body.data as AuthRegisterInput;
+  return res.json(authRegisterV1(data.email, data.password, data.nameFirst, data.nameLast));
 });
 
 // start server
