d18b0a67148bcd0f8b44c03d9193af489e8cc35d
Danny Chan <z5453114@ad.unsw.edu.au>
2023-04-16 18:53:11 +1000

    draft update

--- src/channelRequestor.ts
+++ src/channelRequestor.ts
@@ -2,10 +2,10 @@ import request from 'sync-request';
 import { port, url } from './config.json';
 const SERVER_URL = `${url}:${port}`;
 
-export function channelDetailsV3(token: string, channelId: number) {
+export function requestChannelDetailsV3(token: string, channelId: number) {
   const res = request(
     'GET',
-    SERVER_URL + 'channel/details/v3',
+    SERVER_URL + '/channel/details/v3',
     {
       json: {
         token: token,
@@ -16,10 +16,10 @@ export function channelDetailsV3(token: string, channelId: number) {
   return { status: res.statusCode, returnObj: JSON.parse(res.getBody() as string) };
 }
 
-export function channelJoinV3(token: string, channelId: number) {
+export function requestChannelJoinV3(token: string, channelId: number) {
   const res = request(
     'POST',
-    SERVER_URL + 'channel/join/v3',
+    SERVER_URL + '/channel/join/v3',
     {
       json: {
         token: token,
@@ -30,10 +30,10 @@ export function channelJoinV3(token: string, channelId: number) {
   return { status: res.statusCode, returnObj: JSON.parse(res.getBody() as string) };
 }
 
-export function channelInviteV3(authUserId: number, channelId: number, uId: number) {
+export function requestChannelInviteV3(authUserId: number, channelId: number, uId: number) {
   const res = request(
     'POST',
-    SERVER_URL + 'channel/invite/v3',
+    SERVER_URL + '/channel/invite/v3',
     {
       json: {
         authUserId: authUserId,
@@ -45,10 +45,10 @@ export function channelInviteV3(authUserId: number, channelId: number, uId: numb
   return { status: res.statusCode, returnObj: JSON.parse(res.getBody() as string) };
 }
 
-export function channelMessagesV2(token: string, channelId: number, start: number) {
+export function requestChannelMessagesV3(token: string, channelId: number, start: number) {
   const res = request(
     'GET',
-    SERVER_URL + 'channel/messages/v3',
+    SERVER_URL + '/channel/messages/v3',
     {
       json: {
         token: token,
